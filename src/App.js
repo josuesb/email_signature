84f9636652b3580640f2ad2465e0299a688f3641
Josue S <josb2612@gmail.com>
2022-01-15 23:14:08 -0600

    UI Cleaned to be all bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Card from "./components/Card/Card"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 
 
 
@@ -68,11 +69,42 @@ class App extends Component {
 
         <Navbar></Navbar>
         <Container className="pt-5">
-          <input type="text" onChange={this.updateName} placeholder="Name" />{" "}
-          <input type="text" onChange={this.updateRole} placeholder="Role" />{" "}
-          <input type="text" onChange={this.updatePhone} placeholder="Phone" />{" "}
-          <input type="text" onChange={this.updateURL} placeholder="Website" />{" "}
-          <br></br>
+          <Row>
+            <Col>
+              <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" onChange={this.updateName} />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3" controlId="role">
+                <Form.Label>Role</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" onChange={this.updateRole} />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3" controlId="phone">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control type="text" placeholder="Enter your name" onChange={this.updatePhone} />
+              </Form.Group>
+              <Form.Text id="passwordHelpBlock" muted>
+                Phone numbers must be added in an international format.
+              </Form.Text>
+            </Col>
+            <Col>
+              <Form.Group className="mb-3" controlId="phone">
+                <Form.Label>Website</Form.Label>
+                <Form.Control type="text" placeholder="Enter your website URL" onChange={this.updateURL} />
+              </Form.Group>
+              <Form.Text id="passwordHelpBlock" muted>
+                Please include the full URL of your website, including protocols like https.
+              </Form.Text>
+            </Col>
+
+          </Row>
+
+
+
           <br></br>
           <Row className="">
             <Col><Card cardName="No website URL" cardId="copy1" innerContentIndex={0} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
@@ -82,9 +114,25 @@ class App extends Component {
             <Col><Card cardName="Phone number and website" cardId="copy3" innerContentIndex={2} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
             <Col><Card cardName="Simple" cardId="copy4" innerContentIndex={3} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
           </Row>
+          {/**ADDED TO TEST SCROLL - START *}
+          <Row className="pt-4">
+            <Col><Card cardName="Phone number and website" cardId="copy3" innerContentIndex={2} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
+            <Col><Card cardName="Simple" cardId="copy4" innerContentIndex={3} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
+          </Row>
 
+          <Row className="pt-4">
+            <Col><Card cardName="Phone number and website" cardId="copy3" innerContentIndex={2} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
+            <Col><Card cardName="Simple" cardId="copy4" innerContentIndex={3} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
+          </Row>
 
+          <Row className="pt-4">
+            <Col><Card cardName="Phone number and website" cardId="copy3" innerContentIndex={2} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
+            <Col><Card cardName="Simple" cardId="copy4" innerContentIndex={3} name={this.state.name} role={this.state.role} phone={this.state.phone} url={this.state.url}></Card></Col>
+          </Row>
+
+          {/**ADDED TO TEST SCROLL - END */}
         </Container>
+        <br></br>
       </div>
 
     )
